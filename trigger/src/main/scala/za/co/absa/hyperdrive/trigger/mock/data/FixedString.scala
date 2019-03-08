aeba34c236c244967a37d7d69897afc8003e2fdd
Felipe Melo <felipesmmelo@gmail.com>
2019-03-08 12:41:13 -0300

    Separating components into Maven modules

---
+++ trigger/src/main/scala/za/co/absa/hyperdrive/trigger/mock/data/FixedString.scala
@@ -0,0 +1,34 @@
+/*
+ *
+ * Copyright 2019 ABSA Group Limited
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package za.co.absa.hyperdrive.trigger.mock.data
+
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericFixed
+
+object FixedString {
+  def getClassName: String = new FixedString("").getClass.getName
+}
+
+/**
+  * Utility class for writing Avro fixed fields.
+  */
+class FixedString(value: String) extends GenericFixed {
+  override def getSchema: Schema = null
+  override def bytes: Array[Byte] = value.getBytes
+}
---
+++ trigger/src/main/scala/za/co/absa/hyperdrive/trigger/utils/PayloadPrinter.scala
@@ -0,0 +1,44 @@
+/*
+ *
+ * Copyright 2019 ABSA Group Limited
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package za.co.absa.hyperdrive.trigger.utils
+
+import org.apache.spark.sql.SparkSession
+
+object PayloadPrinter {
+
+  val FORMAT_PARQUET = "parquet"
+  val FORMAT_JSON = "json"
+
+  def main(args: Array[String]): Unit = {
+    showContent("/tmp/HYPERDRIVE_PAYLOAD/dest2", FORMAT_JSON)
+  }
+
+  def showContent(sourceDir: String, format: String): Unit = {
+    println(s"Going to print Parquet content from '$sourceDir'")
+    val spark = SparkSession.builder().appName("ParquetPresenter").master("local[*]").getOrCreate()
+
+    val data = format match {
+      case FORMAT_JSON => spark.read.json(sourceDir)
+      case FORMAT_PARQUET => spark.read.parquet(sourceDir)
+    }
+
+    println(s"TOTAL messages in '$sourceDir': ${data.count()}")
+    data.show(false)
+  }
+}
