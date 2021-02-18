f7d2dc182a2c97cc06cb6085a16830c0c9977afc
Kevin Wallimann <kevin.wallimann@absa.africa>
2021-02-18 15:06:22 +0100

    Add module for spark 3 release package

---
+++ hyperdrive-release_spark-3/src/main/scala/SourceJarPlaceholder.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+  * This object is only a placeholder to make sure that a sources jar and a javadoc jar will be generated for this module.
+  * This is a requirement of maven central. The purpose of this module is to have a separate module for the fat-jar
+  * such that users who want to add a dependency on the driver module are not forced to have the fat-jar in their dependencies.
+  */
+object SourceJarPlaceholder
