8281079cab3edd7bb96bb8a723f75917a0a15d0d
Kevin Wallimann <kevin.wallimann@absa.africa>
2021-02-22 10:48:03 +0100

    Reintroduce separate build profile for spark 2

--- deploy.sh
+++ deploy.sh
@@ -17,15 +17,22 @@ fi
 # RELEASE
 RELEASE_BRANCH="release/v${RELEASE_VERSION}"
 git checkout -b ${RELEASE_BRANCH}
-mvn -B release:prepare -Pscala-2.12,spark-3 -DreleaseVersion=${RELEASE_VERSION}
+mvn -B release:prepare -Pscala-2.12,spark-2,spark-3 -DreleaseVersion=${RELEASE_VERSION}
 
 # DEPLOYMENT
 git checkout tags/v${RELEASE_VERSION}
-echo "DEPLOY SCALA-2.11 ARTIFACTS"
-mvn scala-cross-build:change-version -Pscala-2.11
-mvn clean deploy -DskipTests -Pdeploy,scala-2.11
+echo "DEPLOY SPARK-2 / SCALA-2.11 ARTIFACTS"
+mvn scala-cross-build:change-version -Pscala-2.11,spark-2
+mvn clean deploy -DskipTests -Pdeploy,scala-2.11,spark-2
 mvn scala-cross-build:restore-version
 git clean -f
 
-echo "DEPLOY SCALA-2.12 ARTIFACTS"
+echo "DEPLOY SPARK-2 / SCALA-2.12 ARTIFACTS"
+mvn clean deploy -DskipTests -Pdeploy,scala-2.12,spark-2
+
+echo "DEPLOY SPARK-3 ARTIFACTS"
+cd compatibility_spark-3
+mvn clean deploy -DskipTests -Pdeploy,scala-2.12,spark-3
+cd ..
+cd hyperdrive-release_spark-3
 mvn clean deploy -DskipTests -Pdeploy,scala-2.12,spark-3
