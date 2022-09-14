3ef03f690df8f33d4e06efd22ca6067b0ad5b9e3
jozefbakus <jozef.bakus@gmail.com>
2022-09-14 17:58:52 +0200

    POC DELTA

---
+++ ingestor-default/src/main/scala/za/co/absa/hyperdrive/ingestor/implementation/writer/deltaCDCToSnapshot/DeltaCDCToSnapshotWriter.scala
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.hyperdrive.ingestor.implementation.writer.deltaCDCToSnapshot
+
+import io.delta.tables.{DeltaMergeBuilder, DeltaTable}
+import org.apache.commons.configuration2.Configuration
+import org.apache.commons.lang3.StringUtils
+import org.apache.spark.sql.catalyst.expressions.objects.AssertNotNull
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.streaming.{OutputMode, StreamingQuery, Trigger}
+import org.apache.spark.sql.{Column, DataFrame, Row, functions}
+import org.slf4j.LoggerFactory
+import za.co.absa.hyperdrive.compatibility.provider.CompatibleDeltaIngestorProvider
+import za.co.absa.hyperdrive.ingestor.api.utils.{ConfigUtils, StreamWriterUtil}
+import za.co.absa.hyperdrive.ingestor.api.writer.{StreamWriter, StreamWriterFactory, StreamWriterProperties}
+
+private[writer] class DeltaCDCToSnapshotWriter(destination: String,
+                                        trigger: Trigger,
+                                        checkpointLocation: String,
+                                        partitionColumns: Seq[String],
+                                        keyColumn: String,
+                                        opColumn: String,
+                                        deletedValue: String,
+                                        sortColumns: Seq[String],
+                                        sortColumnsCustomOrder: Map[String, Seq[String]],
+                                        val extraConfOptions: Map[String, String]) extends StreamWriter {
+  private val logger = LoggerFactory.getLogger(this.getClass)
+  if (StringUtils.isBlank(destination)) {
+    throw new IllegalArgumentException(s"Invalid DELTA destination: '$destination'")
+  }
+
+  override def write(dataFrame: DataFrame): StreamingQuery = {
+    CompatibleDeltaIngestorProvider.isDeltaSupported()
+    dataFrame.writeStream
+      .trigger(trigger)
+      .outputMode(OutputMode.Append())
+      .option(StreamWriterProperties.CheckpointLocation, checkpointLocation)
+      .options(extraConfOptions)
+      .foreachBatch((df: DataFrame, batchId: Long) => {
+        if(!DeltaTable.isDeltaTable(df.sparkSession, destination)) {
+          df.sparkSession
+            .createDataFrame(df.sparkSession.sparkContext.emptyRDD[Row], df.schema)
+            .write
+            .format("delta")
+            .mode("overwrite")
+            .option("overwriteSchema", "true")
+            .partitionBy(partitionColumns :_*)
+            .save(destination)
+        }
+
+        logger.info(s"Writing batchId: $batchId")
+
+        val sortFieldsPrefix = "tmp_hyperdrive_"
+
+        val dataFrameWithSortColumns = getDataFrameWithSortColumns(df, sortFieldsPrefix)
+
+        val originalFieldNames = df.schema.fieldNames.mkString(",")
+        val sortColumnsWithPrefix = sortColumns.map(sortColumn => s"$sortFieldsPrefix$sortColumn")
+
+        val latestChangeForEachKey = dataFrameWithSortColumns
+          .selectExpr(s"$keyColumn", s"struct(${sortColumnsWithPrefix.mkString(",")}, $originalFieldNames) as otherCols" )
+          .groupBy(s"$keyColumn")
+          .agg(functions.max("otherCols").as("latest"))
+          .filter(col("latest").isNotNull)
+          .withColumn("latest", new Column(AssertNotNull(col("latest").expr)))
+          .selectExpr( "latest.*")
+          .drop(sortColumnsWithPrefix :_*)
+
+        generateDeltaMerge(latestChangeForEachKey).execute()
+      })
+      .start()
+  }
+
+  private def generateDeltaMerge(latestChanges: DataFrame): DeltaMergeBuilder = {
+    val initialDeltaBuilder = DeltaTable
+      .forPath(destination)
+      .as("currentTable")
+      .merge(latestChanges.as("changes"), s"currentTable.$keyColumn = changes.$keyColumn")
+      .whenMatched(s"changes.$opColumn = '$deletedValue'")
+      .delete()
+
+    val deltaBuilderWithSortColumns = sortColumns.foldLeft(initialDeltaBuilder) { (builder, sortColumn) =>
+      val order = sortColumnsCustomOrder.getOrElse(sortColumn, Seq.empty[String])
+      order match {
+        case o if o.isEmpty =>
+          builder
+            .whenMatched(s"changes.$sortColumn > currentTable.$sortColumn")
+            .updateAll()
+        case o =>
+          val orderString = o.mkString("#")
+          builder
+            .whenMatched(s"""locate(changes.$sortColumn, "$orderString") > locate(currentTable.$sortColumn, "$orderString")""")
+            .updateAll()
+      }
+    }
+
+    deltaBuilderWithSortColumns
+      .whenNotMatched(s"changes.$opColumn != '$deletedValue'")
+      .insertAll()
+  }
+
+  private def getDataFrameWithSortColumns(dataFrame: DataFrame, sortFieldsPrefix: String): DataFrame = {
+    sortColumns.foldLeft(dataFrame) { (df, sortColumn) =>
+      val order = sortColumnsCustomOrder.getOrElse(sortColumn, Seq.empty[String])
+      order match {
+        case o if o.isEmpty =>
+          df.withColumn(s"$sortFieldsPrefix$sortColumn", col(s"$sortColumn"))
+        case o =>
+          val orderString = o.mkString("#")
+          df.withColumn(s"$sortFieldsPrefix$sortColumn", functions.expr(s"""locate(value, "$orderString")"""))
+      }
+    }
+  }
+
+  def getDestination: String = destination
+}
+
+object DeltaCDCToSnapshotWriter extends StreamWriterFactory with DeltaCDCToSnapshotWriterAttributes {
+
+  def apply(config: Configuration): StreamWriter = {
+    val destinationDirectory = getDestinationDirectory(config)
+    val trigger = StreamWriterUtil.getTrigger(config)
+    val checkpointLocation = StreamWriterUtil.getCheckpointLocation(config)
+    val extraOptions = getExtraOptions(config)
+    val partitionColumns = ConfigUtils.getSeqOrNone(KEY_PARTITION_COLUMNS, config).getOrElse(Seq())
+
+    val keyColumn = ConfigUtils.getOrThrow(KEY_KEY_COLUMN, config)
+    val opColumn = ConfigUtils.getOrThrow(KEY_OP_COLUMN, config)
+    val deletedValue = ConfigUtils.getOrThrow(KEY_OP_DELETED_VALUE, config)
+    val sortColumns = ConfigUtils.getSeqOrThrow(KEY_SORT_COLUMNS, config)
+    val sortColumnsCustomOrder =  ConfigUtils.getMapOrEmpty(KEY_SORT_COLUMNS_CUSTOM_ORDER, config)
+
+
+    LoggerFactory.getLogger(this.getClass).info(s"Going to create DeltaStreamWriter instance using: " +
+      s"destination directory='$destinationDirectory', trigger='$trigger', checkpointLocation='$checkpointLocation', extra options='$extraOptions'")
+
+    new DeltaCDCToSnapshotWriter(destinationDirectory, trigger, checkpointLocation, partitionColumns, keyColumn, opColumn, deletedValue, sortColumns, sortColumnsCustomOrder, extraOptions)
+  }
+
+  def getDestinationDirectory(configuration: Configuration): String = ConfigUtils.getOrThrow(KEY_DESTINATION_DIRECTORY, configuration, errorMessage = s"Destination directory not found. Is '$KEY_DESTINATION_DIRECTORY' defined?")
+
+  def getExtraOptions(configuration: Configuration): Map[String, String] = ConfigUtils.getPropertySubset(configuration, KEY_EXTRA_CONFS_ROOT)
+
+  override def getExtraConfigurationPrefix: Option[String] = Some(KEY_EXTRA_CONFS_ROOT)
+}
---
+++ ingestor-default/src/main/scala/za/co/absa/hyperdrive/ingestor/implementation/writer/deltaCDCToSnapshot/DeltaCDCToSnapshotWriterAttributes.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.hyperdrive.ingestor.implementation.writer.deltaCDCToSnapshot
+
+import za.co.absa.hyperdrive.ingestor.api.writer.StreamWriterCommonAttributes
+import za.co.absa.hyperdrive.ingestor.api.{HasComponentAttributes, PropertyMetadata}
+
+trait DeltaCDCToSnapshotWriterAttributes extends HasComponentAttributes {
+  private val rootFactoryConfKey = "writer.delta"
+  val KEY_DESTINATION_DIRECTORY = s"$rootFactoryConfKey.destination.directory"
+  val KEY_EXTRA_CONFS_ROOT = s"$rootFactoryConfKey.options"
+  val KEY_PARTITION_COLUMNS = s"$rootFactoryConfKey.partition.columns"
+  val KEY_KEY_COLUMN = s"$rootFactoryConfKey.key.column"
+  val KEY_OP_COLUMN = s"$rootFactoryConfKey.op.column"
+  val KEY_OP_DELETED_VALUE = s"$rootFactoryConfKey.op.deleted.value"
+  val KEY_SORT_COLUMNS = s"$rootFactoryConfKey.sortColumns"
+  val KEY_SORT_COLUMNS_CUSTOM_ORDER = s"$rootFactoryConfKey.sortColumns.customOrder"
+
+  override def getName: String = "Delta Stream Writer"
+
+  override def getDescription: String = "This writer saves ingested data in Delta format on a filesystem (e.g. HDFS)"
+
+  override def getProperties: Map[String, PropertyMetadata] = Map(
+    KEY_DESTINATION_DIRECTORY -> PropertyMetadata("Destination directory", Some("A path to a directory"), required = true),
+    KEY_PARTITION_COLUMNS -> PropertyMetadata("Partition columns", Some("Comma-separated list of columns to partition by"), required = false),
+    KEY_KEY_COLUMN -> PropertyMetadata("Key column", Some("A column with unique entity identifier"), required = true),
+    KEY_OP_COLUMN -> PropertyMetadata("Op column", Some("A column containing values marking a record with an operation"), required = true),
+    KEY_OP_DELETED_VALUE -> PropertyMetadata("Delete value for op column", Some("The value marking a record for deletion in the op column"), required = true),
+    KEY_SORT_COLUMNS -> PropertyMetadata("Sort columns", Some("A list of columns used to sort rows based on precedence"), required = true),
+    KEY_SORT_COLUMNS_CUSTOM_ORDER -> PropertyMetadata("Columns' custom order", Some("A column's custom order"), required = false),
+    StreamWriterCommonAttributes.keyTriggerProcessingTime -> StreamWriterCommonAttributes.triggerProcessingTimeMetadata,
+    StreamWriterCommonAttributes.keyCheckpointBaseLocation -> StreamWriterCommonAttributes.checkpointBaseLocation
+  )
+}
