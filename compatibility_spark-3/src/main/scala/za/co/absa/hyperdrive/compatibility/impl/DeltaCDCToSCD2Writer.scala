5352d06df02975604daf76782df3b5c3ee43d8ed
jozefbakus <jozef.bakus@gmail.com>
2022-09-22 08:26:19 +0200

    Delta cdc to scd2 writer

---
+++ compatibility_spark-3/src/main/scala/za/co/absa/hyperdrive/compatibility/impl/DeltaCDCToSCD2Writer.scala
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.hyperdrive.compatibility.impl
+
+import za.co.absa.hyperdrive.compatibility.api.{CompatibleDeltaCDCToSCD2Writer, DeltaCDCToSCD2WriterConfiguration}
+import io.delta.tables.{DeltaMergeBuilder, DeltaTable}
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.sql.{Column, DataFrame, Row, SaveMode, SparkSession, functions}
+import org.apache.spark.sql.catalyst.expressions.objects.AssertNotNull
+import org.apache.spark.sql.functions.{col, lag, lit, row_number}
+import org.apache.spark.sql.streaming.{OutputMode, StreamingQuery}
+import org.slf4j.LoggerFactory
+import org.apache.spark.sql.types.{BooleanType, TimestampType}
+
+import java.net.URI
+
+class DeltaCDCToSCD2Writer(configuration: DeltaCDCToSCD2WriterConfiguration) extends CompatibleDeltaCDCToSCD2Writer {
+  private val logger = LoggerFactory.getLogger(this.getClass)
+  private val STRING_SEPARATOR = "#$@"
+  private val START_DATE_COLUMN = "_start_date"
+  private val END_DATE_COLUMN = "_end_date"
+  private val IS_CURRENT_COLUMN = "_is_current"
+
+  if(configuration.precombineColumnsCustomOrder.values.flatten.toSeq.contains(STRING_SEPARATOR)) {
+    throw new IllegalArgumentException(s"Precombine columns custom order cannot contain string separator: $STRING_SEPARATOR")
+  }
+
+  override def write(dataFrame: DataFrame): StreamingQuery = {
+    dataFrame.writeStream
+      .trigger(configuration.trigger)
+      .outputMode(OutputMode.Append())
+      .option(configuration.checkpointLocationPropName, configuration.checkpointLocation)
+      .options(configuration.extraConfOptions)
+      .foreachBatch((df: DataFrame, batchId: Long) => {
+        if(!DeltaTable.isDeltaTable(df.sparkSession, configuration.destination)) {
+          if(isDirEmptyOrDoesNotExist(df.sparkSession, configuration.destination)) {
+            logger.info(s"Destination: ${configuration.destination} is not a delta table. Creating new delta table.")
+            df.sparkSession
+              .createDataFrame(df.sparkSession.sparkContext.emptyRDD[Row], df.schema.add(START_DATE_COLUMN, TimestampType, false).add(END_DATE_COLUMN, TimestampType, true).add(IS_CURRENT_COLUMN, BooleanType, false))
+              .write
+              .format("delta")
+              .mode(SaveMode.Overwrite)
+              .option("overwriteSchema", "true")
+              .partitionBy(configuration.partitionColumns :_*)
+              .save(configuration.destination)
+          } else {
+            throw new IllegalArgumentException(s"Could not create new delta table. Directory ${configuration.destination} is not empty!")
+          }
+        }
+        logger.info(s"Writing batchId: $batchId")
+
+        val sortFieldsPrefix = "_tmp_hyperdrive_"
+
+        val dataFrameWithSortColumns = getDataFrameWithSortColumns(df, sortFieldsPrefix)
+
+        val fieldNames = df.schema.fieldNames
+        val originalFieldNames = df.schema.fieldNames.mkString(",")
+        val sortColumnsWithPrefix = configuration.precombineColumns.map(precombineColumn => s"$sortFieldsPrefix$precombineColumn")
+
+        val uniqueChangesForEachKeyAndTimestamp = dataFrameWithSortColumns
+          .selectExpr(s"${configuration.keyColumn}", s"${configuration.timeStampColumn}", s"struct(${sortColumnsWithPrefix.mkString(",")}, $originalFieldNames) as otherCols" )
+          .groupBy(s"${configuration.keyColumn}", s"${configuration.timeStampColumn}")
+          .agg(functions.max("otherCols").as("latest"))
+          .filter(col("latest").isNotNull)
+          .withColumn("latest", new Column(AssertNotNull(col("latest").expr)))
+          .selectExpr("latest.*")
+          .drop(sortColumnsWithPrefix :_*)
+
+        val stagedDataFrame = getStagedDataFrame(uniqueChangesForEachKeyAndTimestamp)
+
+
+        generateDeltaMerge(stagedDataFrame, fieldNames).execute()
+      })
+      .start()
+  }
+
+  private def generateDeltaMerge(latestChanges: DataFrame, originalFieldNames: Seq[String]): DeltaMergeBuilder = {
+    val initialDeltaBuilder = DeltaTable
+      .forPath(configuration.destination)
+      .as("currentTable")
+      .merge(latestChanges.as("changes"), s"currentTable._is_current = true AND currentTable.${configuration.keyColumn} = changes._mergeKey") // AND currentTable._is_current = true")
+      .whenMatched("currentTable.A_TIMSTAMP < changes.A_TIMSTAMP")
+        .update(
+          Map(
+            "_end_date" -> col("changes.A_TIMSTAMP"),
+            "_is_current" -> lit(false)
+          )
+        )
+
+    val deltaBuilderWithSortColumns = configuration.precombineColumns.foldLeft(initialDeltaBuilder) { (builder, precombineColumn) =>
+          val order = configuration.precombineColumnsCustomOrder.getOrElse(precombineColumn, Seq.empty[String])
+          order match {
+            case o if o.isEmpty =>
+              builder
+                .whenMatched(s"currentTable.$precombineColumn < changes.$precombineColumn")
+                .update(
+                  Map(
+                    "_end_date" -> col("changes.A_TIMSTAMP"),
+                    "_is_current" -> lit(false)
+                  )
+                )
+            case o =>
+              val orderString = o.mkString(STRING_SEPARATOR)
+              builder
+                .whenMatched(s"""locate(currentTable.$precombineColumn, "$orderString") < locate(changes.$precombineColumn, "$orderString")""")
+                .update(
+                  Map(
+                    "_end_date" -> col("changes.A_TIMSTAMP"),
+                    "_is_current" -> lit(false)
+                  )
+                )
+          }
+        }
+    deltaBuilderWithSortColumns
+      .whenMatched()
+      .delete()
+      .whenNotMatched("changes._mergeKey is NOT NULL")
+      .insert(
+        originalFieldNames.map( c =>
+          c -> col(s"changes.$c")
+        ).toMap ++ Map("_start_date" -> col("changes._start_date"),  "_end_date" -> col("changes._end_date"), "_is_current" -> col("changes._is_current"))
+      )
+  }
+
+  private def getStagedDataFrame(dataFrame: DataFrame): DataFrame = {
+    val idWindowDesc = org.apache.spark.sql.expressions.Window.partitionBy(configuration.keyColumn).orderBy(col(configuration.timeStampColumn).desc)
+    val insertRows = dataFrame
+      .withColumn("_start_date", col(configuration.timeStampColumn))
+      .withColumn("_end_date", lag("_start_date", 1, null).over(idWindowDesc))
+      .withColumn("_mergeKey", lit(null))
+      .withColumn("_end_date", functions.when(col(configuration.operationColumn).equalTo(configuration.operationDeleteValue), col("_start_date")).when(col(configuration.operationColumn).notEqual(configuration.operationDeleteValue), col("_end_date")).otherwise(null))
+      .withColumn("_is_current", functions.when(col("_end_date").isNull, lit(true)).otherwise(lit(false)))
+
+    import dataFrame.sparkSession.implicits._
+    val idWindowAsc = org.apache.spark.sql.expressions.Window.partitionBy(configuration.keyColumn).orderBy(col("_start_date").asc)
+    val updateRows = insertRows.withColumn("_rn", row_number.over(idWindowAsc)).where($"_rn" === 1).drop("_rn").withColumn("_mergeKey", col(configuration.keyColumn))
+
+    insertRows.union(updateRows)
+  }
+
+  private def getDataFrameWithSortColumns(dataFrame: DataFrame, sortFieldsPrefix: String): DataFrame = {
+    configuration.precombineColumns.foldLeft(dataFrame) { (df, precombineColumn) =>
+      val order = configuration.precombineColumnsCustomOrder.getOrElse(precombineColumn, Seq.empty[String])
+      order match {
+        case o if o.isEmpty =>
+          df.withColumn(s"$sortFieldsPrefix$precombineColumn", col(precombineColumn))
+        case o =>
+          val orderString = o.mkString(STRING_SEPARATOR)
+          df.withColumn(s"$sortFieldsPrefix$precombineColumn", functions.expr(s"""locate($precombineColumn, "$orderString")"""))
+      }
+    }
+  }
+
+  private def isDirEmptyOrDoesNotExist(spark: SparkSession, destination: String): Boolean = {
+    implicit val fs: FileSystem = FileSystem.get(new URI(destination), spark.sparkContext.hadoopConfiguration)
+    val path = new Path(destination)
+    if(fs.exists(path)) {
+      if(fs.isDirectory(path)) {
+        !fs.listFiles(path, true).hasNext
+      } else {
+        false
+      }
+    } else {
+      true
+    }
+  }
+}
+
---
+++ ingestor-default/src/main/scala/za/co/absa/hyperdrive/ingestor/implementation/writer/deltacdctoscd2writer/DeltaCDCToSCD2Writer.scala
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.hyperdrive.ingestor.implementation.writer.deltacdctoscd2writer
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.streaming.{StreamingQuery, Trigger}
+import za.co.absa.hyperdrive.ingestor.api.writer.{StreamWriter, StreamWriterFactory, StreamWriterProperties}
+import org.apache.commons.configuration2.Configuration
+import org.apache.commons.lang3.StringUtils
+import org.slf4j.LoggerFactory
+import za.co.absa.hyperdrive.compatibility.api.{CompatibleDeltaCDCToSCD2Writer, DeltaCDCToSCD2WriterConfiguration}
+import za.co.absa.hyperdrive.compatibility.provider.CompatibleDeltaCDCToSCD2WriterProvider
+import za.co.absa.hyperdrive.ingestor.api.utils.{ConfigUtils, StreamWriterUtil}
+
+private[writer] class DeltaCDCToSCD2Writer(destination: String,
+                                           trigger: Trigger,
+                                           checkpointLocation: String,
+                                           partitionColumns: Seq[String],
+                                           keyColumn: String,
+                                           timestampColumn: String,
+                                           operationColumn: String,
+                                           operationDeleteValue: String,
+                                           precombineColumns: Seq[String],
+                                           precombineColumnsCustomOrder: Map[String, Seq[String]],
+                                           val extraConfOptions: Map[String, String]) extends StreamWriter {
+  if (StringUtils.isBlank(destination)) {
+    throw new IllegalArgumentException("Destination must not be blank!")
+  }
+  val compatibleDeltaCDCToSnapshotWriter: CompatibleDeltaCDCToSCD2Writer =
+    CompatibleDeltaCDCToSCD2WriterProvider.provide(
+      DeltaCDCToSCD2WriterConfiguration(
+        StreamWriterProperties.CheckpointLocation,
+        destination,
+        trigger,
+        checkpointLocation,
+        partitionColumns,
+        keyColumn,
+        timestampColumn,
+        operationColumn,
+        operationDeleteValue,
+        precombineColumns,
+        precombineColumnsCustomOrder,
+        extraConfOptions
+      )
+    )
+
+  override def write(dataFrame: DataFrame): StreamingQuery = {
+    compatibleDeltaCDCToSnapshotWriter.write(dataFrame)
+  }
+}
+
+object DeltaCDCToSCD2Writer extends StreamWriterFactory with DeltaCDCToSCD2WriterAttributes {
+
+  def apply(config: Configuration): StreamWriter = {
+    val destinationDirectory = getDestinationDirectory(config)
+    val trigger = StreamWriterUtil.getTrigger(config)
+    val checkpointLocation = StreamWriterUtil.getCheckpointLocation(config)
+    val extraOptions = getExtraOptions(config)
+    val partitionColumns = ConfigUtils.getSeqOrNone(KEY_PARTITION_COLUMNS, config).getOrElse(Seq())
+
+    val keyColumn = ConfigUtils.getOrThrow(KEY_KEY_COLUMN, config)
+    val timestampColumn = ConfigUtils.getOrThrow(KEY_TIMESTAMP_COLUMN, config)
+    val operationColumn = ConfigUtils.getOrThrow(KEY_OPERATION_COLUMN, config)
+    val operationDeleteValue = ConfigUtils.getOrThrow(KEY_OPERATION_DELETED_VALUE, config)
+    val precombineColumns = ConfigUtils.getSeqOrThrow(KEY_PRECOMBINE_COLUMNS, config)
+    val precombineColumnsCustomOrder =  ConfigUtils.getMapOrEmpty(KEY_PRECOMBINE_COLUMNS_CUSTOM_ORDER, config)
+
+
+    LoggerFactory.getLogger(this.getClass).info(s"Going to create DeltaStreamWriter instance using: " +
+      s"destination directory='$destinationDirectory', trigger='$trigger', checkpointLocation='$checkpointLocation', " +
+      s"partition columns='$partitionColumns', key column='$keyColumn', timestamp column='$timestampColumn', operation column='$operationColumn', " +
+      s"operation delete value='$operationDeleteValue', precombine columns='$precombineColumns', " +
+      s"precombine columns custom order='$precombineColumnsCustomOrder', extra options='$extraOptions'")
+
+    new DeltaCDCToSCD2Writer(destinationDirectory, trigger, checkpointLocation, partitionColumns, keyColumn, timestampColumn, operationColumn, operationDeleteValue, precombineColumns, precombineColumnsCustomOrder, extraOptions)
+  }
+
+  def getDestinationDirectory(configuration: Configuration): String = ConfigUtils.getOrThrow(KEY_DESTINATION_DIRECTORY, configuration, errorMessage = s"Destination directory not found. Is '$KEY_DESTINATION_DIRECTORY' defined?")
+
+  def getExtraOptions(configuration: Configuration): Map[String, String] = ConfigUtils.getPropertySubset(configuration, KEY_EXTRA_CONFS_ROOT)
+
+  override def getExtraConfigurationPrefix: Option[String] = Some(KEY_EXTRA_CONFS_ROOT)
+}
